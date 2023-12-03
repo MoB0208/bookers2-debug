a782d6fa02d6be70082bca7283cdea055f192152
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-03 21:49:23 +0000

    [oyokadai3]完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
 
   get 'favorites/create'
   get 'favorites/destroy'
-  devise_for :users
+  
   root to: "homes#top"
   get "home/about" => "homes#about"
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
