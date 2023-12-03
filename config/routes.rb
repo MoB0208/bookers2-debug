c2f4054216fb3ecc8b2f5614b718cafd07ad97a3
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-03 22:39:14 +0000

    [oyokadai4]モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
 
   get 'favorites/create'
   get 'favorites/destroy'
-  
+
   root to: "homes#top"
   get "home/about" => "homes#about"
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
